9ee659a5d097c21a4db310abeeed2fc7e5e1f949
Technorahmon <Abdulrahmanmuhialdeen@hotmail.com>
2022-07-03 18:37:36 +0300

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace BulkyBookWeb.Models;
+
+    public class Category
+    {   
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+

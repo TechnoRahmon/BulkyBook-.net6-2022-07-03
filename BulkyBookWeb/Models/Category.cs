fe5f43b094ab0603f6f5acbd3c916c574385a6e5
Technorahmon <Abdulrahmanmuhialdeen@hotmail.com>
2022-07-03 19:54:11 +0300

    fetch data from database

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,10 +1,12 @@
-﻿namespace BulkyBookWeb.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
 
     public class Category
     {   
-        
+        [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }

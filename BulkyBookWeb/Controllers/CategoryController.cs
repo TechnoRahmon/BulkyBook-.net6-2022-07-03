c03b3f361f87bdc2a73b4926c0ebfff1475e8033
Technorahmon <Abdulrahmanmuhialdeen@hotmail.com>
2022-07-04 16:30:55 +0300

    Edit Category feature

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -43,5 +43,45 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if ( id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            
+            // how to use firstOrDefualt
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            // custom validation 
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("displayorder", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
